b4fe325a77441f91b81df1bb0177ea4c76f51892
joahangf11 <joahangarciaf@hotmail.com>
2023-04-08 00:44:39 -0600

    Add exercises 5 and 6, also deadline was modified.

--- HOfunctions.clj
+++ HOfunctions.clj
@@ -1,6 +1,6 @@
 ;----------------------------------------------------------
 ; Problem Set #4: Higher-Order Functions
-; Date: March 31, 2023.
+; Date: April 12, 2023.
 ; Authors:
 ;          A01748222 Joahan Javier Garcia Fernandez
 ;          A0******* Benjamín Cruz
@@ -15,9 +15,11 @@
 
 ;Exercises
 
-
 ;#1
-
+(defn argswap
+  " takes as input a two argument function f and returns a new function that behaves like f but with the order of its two arguments swapped"
+  [f]
+  (fn [x y] (f y x)))
 ;#2
 (defn there-exists-one
   " Returns true if there is exactly one element in s hat satisfies pred?, otherwise returns false."
@@ -34,9 +36,46 @@
       (eq-fun x (vct i)) i
       :else (recur (inc i)))))
 
+;#4
+(defn deriv
+  "Takes f and h as its input which represents the derivative of f given a certain value of h"
+  [f h]
+  (fn [x] (/ (- (f(+ x h)) (f x)) h)))
 
+;5
+(defn newton
+  "takes f and n as its inputs, and returns the corresponding value of x"
+  [f n]
+  (if (zero? n)
+    0
+    (let [x-n-1 (newton f (dec n))]
+      (- x-n-1
+         (/ (f x-n-1)
+            ((deriv f 0.0001) x-n-1))))))
 
+;6
+(defn integral
+  "that takes as inputs a, b, n, and f. It returns the value of the integral, using Simpson’s rule."
+  [a b n f]
+  (let [h (/ (- b a) n)
+        v1 (f a)
+        v3 (f b) ; Lo mismo que evaluarlo al final (+ a (* n h))
+        sip (loop [acc 0
+                   i 0]
+              (if (> i (/ (- n 2) 2))
+                acc
+                (recur (+ acc (f (+ a (* (+ (* 2 i) 1) h)))) (inc i))))
+        sp (loop [acc 0
+                  i 1]
+             (if (> i (/ (- n 2) 2))
+               acc
+               (recur (+ acc (f (+ a (* (* 2 i) h)))) (inc i))))]
+    (*
+      (/ h 3)
+      (+
+        v1 v3 (* 4 sip) (* 2 sp)))))
 
+;7
 
 
 ;Test
@@ -80,6 +119,47 @@
               69 97 28 32 17 18 96]
              70
              #(<= (abs (- %1 %2)) 1)))))
+
+(defn f [x] (* x x x))
+(def df (deriv f 0.001))
+(def ddf (deriv df 0.001))
+(def dddf (deriv ddf 0.001))
+
+(deftest test-deriv
+  (is (approx= 75 (df 5) 0.05))
+  (is (approx= 30 (ddf 5) 0.05))
+  (is (approx= 6 (dddf 5) 0.05)))
+
+
+(deftest test-newton
+  (is (approx= 10.0
+               (newton (fn [x] (- x 10))
+                       1)
+               0.00001))
+  (is (approx= -0.5
+               (newton (fn [x] (+ (* 4 x) 2))
+                       1)
+               0.00001))
+  (is (approx= -1.0
+               (newton (fn [x] (+ (* x x x) 1))
+                       50)
+               0.00001))
+  (is (approx= -1.02987
+               (newton (fn [x] (+ (Math/cos x)
+                                  (* 0.5 x)))
+                       5)
+               0.00001)))
+
+(deftest test-integral
+  (is (= 1/4 (integral 0 1 10 (fn [x] (* x x x)))))
+  (is (= 21/4
+         (integral 1 2 10
+                   (fn [x]
+                     (integral 3 4 10
+                               (fn [y]
+                                 (* x y))))))))
+
+
 
 ;Run tests
 (run-tests)
