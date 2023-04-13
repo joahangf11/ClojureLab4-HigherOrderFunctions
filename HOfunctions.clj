885b7ddeacb9a36bb626ed384c5ac74d6ff7ec4b
joahangf11 <joahangarciaf@hotmail.com>
2023-04-12 21:23:57 -0600

    The lab has been completed in its totality.

--- HOfunctions.clj
+++ HOfunctions.clj
@@ -3,8 +3,8 @@
 ; Date: April 12, 2023.
 ; Authors:
 ;          A01748222 Joahan Javier Garcia Fernandez
-;          A0******* Benjamín Cruz
-;          A0******* Luis Fernando
+;          A01747811 Benjamín Alejandro Cruz Cervantes
+;          A01754574 Luis Fernando De León Silva
 ;----------------------------------------------------------
 
 ;Namespace
@@ -20,6 +20,7 @@
   " takes as input a two argument function f and returns a new function that behaves like f but with the order of its two arguments swapped"
   [f]
   (fn [x y] (f y x)))
+
 ;#2
 (defn there-exists-one
   " Returns true if there is exactly one element in s hat satisfies pred?, otherwise returns false."
@@ -77,9 +78,38 @@
 
 ;7
 
+(defn binary-search [vct x lt-fun]
+  ;Variable n guarda el número de elementos en el vector
+  (let [n (count vct)]
+    ;Se crea un bucle que recorrera tanto como sea necesario
+    ;Este cuenta con l(indice) y r(el tamaño del vector -1 por que se inicia a contar en 0)
+    (loop [l 0
+           r (dec n)]
+      ;Mientras l(indice) sea menor o igual a r(tamaño actual del vector)
+      (when (<= l r)
+        ;Se pone una variable llamada mid que divide la suma de l y r entre 2 para encontrar la mitad
+        (let [mid (quot (+ l r) 2)]
+          ;Se da una condicion
+          ;Se regresa el mid en caso de que sean iguales x y mid
+          (cond (= x (nth vct mid)) mid
+                ;Se procede a coomparar con la funcion y si se va a la derecha o a la izq
+                (lt-fun x (nth vct mid)) (recur l (dec mid))
+                :else (recur (inc mid) r)))))))
+
 
 ;Test
 
+(deftest test-argswap
+  (is (= '(2 1)
+         ((argswap list) 1 2)))
+  (is (= -7
+         ((argswap -) 10 3)))
+  (is (= 1/4
+         ((argswap /) 8 2)))
+  (is (= '((4 5 6) 1 2 3)
+         ((argswap cons) '(1 2 3) '(4 5 6))))
+  (is (= '(1 0 4 25 100)
+         ((argswap map) '(-1 0 2 5 10) #(* % %)))))
 (deftest test-there-exists-one
   (is (not (there-exists-one pos?
                              ())))
@@ -159,7 +189,43 @@
                                (fn [y]
                                  (* x y))))))))
 
+(def small-list [4 8 15 16 23 42])
 
+(def big-list [0 2 5 10 11 13 16 20 24 26
+               29 30 31 32 34 37 40 43 44
+               46 50 53 58 59 62 63 66 67
+               70 72 77 79 80 83 85 86 94
+               95 96 99])
+
+(def animals ["dog" "dragon" "horse" "monkey" "ox"
+              "pig" "rabbit" "rat" "rooster" "sheep"
+              "snake" "tiger"])
+(defn str<
+  "Returns true if a is less than b, otherwise
+   returns false. Designed to work with strings."
+  [a b]
+  (< (compare a b) 0))
+
+(deftest test-binary-search
+  (is (nil? (binary-search [] 5 <)))
+  (is (= 3 (binary-search small-list 16 <)))
+  (is (= 0 (binary-search small-list 4 <)))
+  (is (= 5 (binary-search small-list 42 <)))
+  (is (nil? (binary-search small-list 7 <)))
+  (is (nil? (binary-search small-list 2 <)))
+  (is (nil? (binary-search small-list 99 <)))
+  (is (= 17 (binary-search big-list 43 <)))
+  (is (= 0 (binary-search big-list 0 <)))
+  (is (= 39 (binary-search big-list 99 <)))
+  (is (nil? (binary-search big-list 12 <)))
+  (is (nil? (binary-search big-list -1 <)))
+  (is (nil? (binary-search big-list 100 <)))
+  (is (= 5 (binary-search animals "pig" str<)))
+  (is (= 0 (binary-search animals "dog" str<)))
+  (is (= 11 (binary-search animals "tiger" str<)))
+  (is (nil? (binary-search animals "elephant" str<)))
+  (is (nil? (binary-search animals "alligator" str<)))
+  (is (nil? (binary-search animals "unicorn" str<))))
 
 ;Run tests
 (run-tests)
